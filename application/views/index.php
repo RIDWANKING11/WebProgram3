fae8a11dfcd945eefb84c76fdef74484b5987d4f
RIDWANKING11 <muhammadridwanking@gmail.com>
2021-10-01 17:53:31 +0700

    Muhammad Ridwan Merubah dibagian index.php

--- application/views/index.php
+++ application/views/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Web Prog II | Merancang Template Sederhana dengan codeignater </title>
-    <link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/stylebuku.css') ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url()">
 </head>
 
 <body>
