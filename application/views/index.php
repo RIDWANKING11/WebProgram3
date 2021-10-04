6f1f995999f26990e60a72249dc1ae7acb7043b5
RIDWANKING11 <muhammadridwanking@gmail.com>
2021-10-04 07:22:34 +0700

    memperbaiki commit index

--- application/views/index.php
+++ application/views/index.php
@@ -19,8 +19,8 @@
                 </hgroup>
                 <nav>
                     <ul>
-                        <li><a href="<?= base_url() . 'index.php/web' ?>">Home</a></li>
-                        <li><a href="<?= base_url() . 'index.php/web/about' ?>">About</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/web' ?>">Home</a></li>
+                        <li><a href="<?php echo base_url() . 'index.php/web/about' ?>">About</a></li>
                     </ul>
                 </nav>
                 <div class="clear"></div>
