ffeeabdfd19c3a0755de48bdcee679ec794e2c99
safaatfikri <fikrisafaat1@gmail.com>
2021-10-03 17:57:07 -0700

    fikri membuat file web.php

---
+++ application/controllers/Web.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') or exit('no direct script acces allowed');
+
+class web extends CI_Controller
+{
+
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->helper('url');
+    }
+
+    public function index()
+    {
+        $data['judul'] = "halaman depan";
+        $this->load->view('v-header', $data);
+        $this->load->view('v-index', $data);
+        $this->load->view('v-footer', $data);
+    }
+    public function about()
+    {
+        $data['judul'] = "halaman about";
+        $this->load->view('v-header', $data);
+        $this->load->view('v-index', $data);
+        $this->load->view('v-footer', $data);
+    }
+}
